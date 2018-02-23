292d946d4c82abef314f11c5adda8ab121cbce2d
Adam <alspdx@gmail.com>
2018-02-23 15:52:11 -0800

    add userprofile component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,11 +1,13 @@
 import Login from './Login';
 import React from 'react';
+import UserProfile from './UserProfile';
 import { Switch, Route } from 'react-router-dom';
 
 function Body() {
   return (
     <div>
       <Switch>
+        <Route path='/user' component={UserProfile} />
         <Route exact path='/' component={Login} />
       </Switch>
     </div>
