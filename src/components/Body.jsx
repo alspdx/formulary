2e28d2e797fc664206832e36c27b216536e536d8
Adam <alspdx@gmail.com>
2018-02-23 16:09:25 -0800

    add error404 component

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,7 @@
-import DetailsList from './DetailsList';
-import Login from './Login';
 import ClientDetails from './ClientDetails';
+import DetailsList from './DetailsList';
+import Error404 from './Error404';
+import Login from './Login';
 import React from 'react';
 import UserProfile from './UserProfile';
 import { Switch, Route } from 'react-router-dom';
@@ -14,6 +15,7 @@ function Body() {
         <Route path='/clients' component={DetailsList} />
         <Route path='/clientdetails' component={ClientDetails} />
         <Route path='/user' component={UserProfile} />
+        <Route component={Error404} />
       </Switch>
     </div>
   );
