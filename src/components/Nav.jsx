80858c89c07c634ade034690b5c91bfac6890878
Adam <alspdx@gmail.com>
2018-03-02 13:31:36 -0800

    style nav items with glamorous

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,11 +8,26 @@ const StyledNav = glamorous.ul({
   },
 );
 
+const navItems = [
+  {
+    title: 'Home',
+    pathName: '/'
+  },
+  {
+    title: 'Clients',
+    pathName: '/clients'
+  },
+  {
+    title: 'Client Details',
+    pathName: '/clientdetails'
+  }
+]
+
 const Nav = () => (
   <StyledNav>
-    <NavItem />
-    <NavItem />
-    <NavItem />
+    {navItems.map(item =>
+      <NavItem key={item.title} title={item.title} pathName={item.pathName} />
+    )}
   </StyledNav>
 )
 
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import glamorous from 'glamorous';
 import colors from './../constants';
+import { Link } from 'react-router-dom';
 
 const StyledLi = glamorous.li({
-    color: colors.white,
     ':nth-child(1n+2)': {
       marginLeft: '16px',
     }
   }
 )
 
-const NavItem = () => (
-  <StyledLi>NavItem</StyledLi>
+const StyledLink = glamorous(Link)({
+  textDecoration: 'none',
+  color: colors.white
+})
+
+const NavItem = ({ title, pathName }) => (
+  <StyledLi><StyledLink to={pathName}>{title}</StyledLink></StyledLi>
 )
 
 export default NavItem;
