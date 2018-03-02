dbfa856824e6f811279adc9068b44887066d0cbb
Adam <alspdx@gmail.com>
2018-03-02 11:24:47 -0800

    remove glamor imports

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import NavItem from './NavItem';
 import glamorous from 'glamorous';
-import { css, select as $ } from 'glamor';
 
 const StyledNav = glamorous.ul({
     display: 'flex',
