58a7e1c18f769be0548d05e72741b577c1e15a13
Adam <alspdx@gmail.com>
2018-03-02 12:38:54 -0800

    style nav items and header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import UserHeading from './UserHeading';
 import Nav from './Nav';
 import glamorous from 'glamorous';
+import colors from './../constants';
 
 const StyledHeader = glamorous.header({
-    backgroundColor: 'yellow',
+    backgroundColor: colors.primary,
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -3,9 +3,9 @@ import glamorous from 'glamorous';
 import colors from './../constants';
 
 const StyledLi = glamorous.li({
+    color: colors.white,
     ':nth-child(1n+2)': {
       marginLeft: '16px',
-      backgroundColor: colors.blush
     }
   }
 )
