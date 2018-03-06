f3ced443c4f47ef73c83cbbb495b3a9b59bd17cb
Adam <alspdx@gmail.com>
2018-03-05 21:28:49 -0800

    add variables file to constants folder

---
+++ src/constants/Variables.js
@@ -0,0 +1,2 @@
+export const borderRadius = '2px';
+export const formPadding = '60px 100px';
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,9 @@
 import * as colors from './Colors';
 import * as shadows from './Shadows';
+import * as variables from './Variables';
 
 export default {
   colors: colors,
-  shadows: shadows
+  shadows: shadows,
+  variables: variables
 };
