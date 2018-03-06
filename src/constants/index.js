6eac318e5a84af5f37a43b40235b3d35d167c0b9
Adam <alspdx@gmail.com>
2018-03-06 11:32:54 -0800

    add firebaseconfig file with credentials

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,11 @@
 import * as colors from './Colors';
 import * as shadows from './Shadows';
 import * as variables from './Variables';
+import firebaseConfig from './firebaseConfig';
 
 export default {
   colors: colors,
   shadows: shadows,
-  variables: variables
+  variables: variables,
+  firebaseConfig: firebaseConfig
 };
